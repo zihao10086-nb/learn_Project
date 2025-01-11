ccd23a7f0be12f15afcef37bec94af95307f70f0
zihao10086-nb <3534193379@qq.com>
2025-01-11 11:15:31 +0800

    Add files via upload

---
+++ 2048/scripts/storage.js
@@ -0,0 +1,24 @@
+// 历史最高分
+const BestScoreKey = '2048BestScore';
+// 方格状态和分数
+const CellStateKey = '2048CellState';
+
+function Storage() {}
+
+// 存储方格状态和分数
+Storage.prototype.setCellState = function({ score, grid ,finalscore}) {
+  window.localStorage.setItem(
+    CellStateKey,
+    JSON.stringify({
+      score,
+      finalscore,
+      grid: grid.serialize()
+    })
+  );
+};
+
+// 获取方格信息
+Storage.prototype.getCellState = function() {
+  const cellState = window.localStorage.getItem(CellStateKey);
+  return cellState ? JSON.parse(cellState) : null;
+};
